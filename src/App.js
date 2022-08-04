91eb56271248d61d02f86230a4bff4a8815cfa4e
Alen Jojo <alenjojo252001@gmail.com>
2022-08-05 00:22:14 +0530

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>
-          Portfolio
-        </h1>
+      <NavBar />  
+
+      <div className="top-[30%] left-[3%] absolute text-left">
+        <div className='text-7xl font-bold'>
+        I build things for EVERYTHING.
+        </div>      
+      <br></br>
+      <div className='w-[55%] font-semibold'>
+      Hands-on and comprehensive knowledge of platform development enterprise architecture
+      ,Quick Learner agile methodologies and web-based applications. Innovative change with a
+      unique mix of high-level of technology in every direction and deep technical expertise.I can 
+      adapt to any field whichever possible.
+      </div>
       </div>
     </div>
   );
