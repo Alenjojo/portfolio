b0e8f6499521211cb932a312b6f45abf14ad4cd6
Alen Jojo <alenjojo252001@gmail.com>
2022-08-05 20:06:49 +0530

    Update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,19 +4,6 @@ import AnimatedText from "../utils/AnimatedText";
 import Pdf from "../files/resume.pdf";
 
 function Body() {
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.5,
-      },
-    },
-  };
-  const item = {
-    hidden: { opacity: 0, y: 100 },
-    show: { opacity: 1, y: 0 },
-  };
   return (
     <motion.div
       initial={{ opacity: 0 }}
