a653cbfd86fc491d1233a58dc8adee3dd3c67d6c
Alen Jojo <alenjojo252001@gmail.com>
2022-08-05 19:10:01 +0530

    Added loader

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import {Route, Routes, useLocation } from 'react-router-dom';
-import Body from './Body';
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Body from "./Body";
 import Experience from "./Experience";
 import Project from "./Project";
-import NavBar from './NavBar';
-import {AnimatePresence} from "framer-motion";
-import About from './About';
+import NavBar from "./NavBar";
+import { AnimatePresence } from "framer-motion";
+import About from "./About";
+import LoadingBar from "./LoadingBar";
 
 function AnimatedRoutes() {
-    const location = useLocation();
-    return (  
-        <AnimatePresence>
-            <NavBar />
-        <Routes location={location} key={location.pathname}> 
-            <Route path="/" element={<> <Body /> </>}/>
-            <Route path="/a" element={<About />}/>
-            <Route path="/e" element={<Experience />}/>
-            <Route path="/p" element={<Project />}/>
-        </Routes>
-        </AnimatePresence>
-    );
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <LoadingBar />
+      <NavBar />
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <>
+              {" "}
+              <Body />{" "}
+            </>
+          }
+        />
+        <Route path="/a" element={<About />} />
+        <Route path="/e" element={<Experience />} />
+        <Route path="/p" element={<Project />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
 export default AnimatedRoutes;
---
+++ src/components/LoadingBar.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import "./LoadingBar.css";
+
+function LoadingBar() {
+  const [isAnimate, setanimate] = useState(true);
+  const [counter, setCounter] = useState(3);
+  //Timer for loading bar
+  useEffect(() => {
+    if (counter <= 0) {
+      setanimate(false);
+      return;
+    }
+    setTimeout(() => {
+      setCounter(counter - 1);
+    }, 1000);
+  }, [counter]);
+  return (
+    <>
+      {isAnimate && (
+        <div class="wrapper">
+          <div class="progressbar">
+            <div class="stylization"></div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default LoadingBar;
