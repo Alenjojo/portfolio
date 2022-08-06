d39b4e7ad265ef29074255c64e94b979d35cca41
Alen Jojo <alenjojo252001@gmail.com>
2022-08-06 16:32:36 +0530

    fixed contact href

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -71,7 +71,7 @@ function NavBar() {
               </li>
               <li>
                 <a
-                  href="mail:alen@alenjojo.com"
+                  href="mailto:alen@alenjojo.com"
                   className="block py-2 pr-4 pl-3 text-fuchsia-700 font-semibold text-lg rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   <AnimatedText
